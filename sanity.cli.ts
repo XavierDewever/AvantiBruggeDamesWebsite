22202a2c4c2258d47ddde50e0f3edc0a6af417d0
Xavier Dewever <xavier.dewever@mylighthouse.com>
2026-05-14 16:34:13 +0200

    Remove GitHub Action deployment

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -5,6 +5,7 @@ export default defineCliConfig({
     projectId: "8xyvn733",
     dataset: "production",
   },
+  studioHost: 'avanti-dames-admin',
   deployment: {
     appId: "k5dxe5c2gg73o5q00efkgmhr",
   },
--- sanity.config.ts
+++ sanity.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
 import { visionTool } from "@sanity/vision";
-import { schemaTypes } from "@/sanity/schemas";
+import { schemaTypes } from "./src/sanity/schemas";
 
 export default defineConfig({
   name: "default",
   title: "Website Studio",
 
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET ?? "production",
+  projectId: "8xyvn733",
+  dataset: "production",
 
   basePath: "/studio",
 
